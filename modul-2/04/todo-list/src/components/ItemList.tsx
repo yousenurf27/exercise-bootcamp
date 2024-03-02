d05a45577ca84462472f26ef8ab37d14ccde8b7b
yousenurf <yosenurf@gmail.com>
2024-03-02 21:35:22 +0700

    feat: exercise todolist

--- modul-2/04/todo-list/src/components/ItemList.tsx
+++ modul-2/04/todo-list/src/components/ItemList.tsx
@@ -3,19 +3,12 @@ import { Button, Checkbox, Wrap } from "@chakra-ui/react"
 import { TItemList } from "../type"
 
 const ItemList = ({ id, title, isDone, onIsDone, onDelete }: TItemList) => {
-  let Checkboxs;
-  if(isDone) {
-    Checkboxs = <Checkbox onChange={() => onIsDone(id)} size={['md', 'lg']} colorScheme='green' defaultChecked>
-        { title }
-      </Checkbox>
-  } else {
-    Checkboxs = <Checkbox onChange={() => onIsDone(id)} size={['md', 'lg']} colorScheme='green'>
-        { title }
-      </Checkbox>
-  }
+
   return (
     <Wrap id={id} justify='space-between' mb='1rem'>
-      {Checkboxs}
+      <Checkbox onChange={() => onIsDone(id)} size={['md', 'lg']} colorScheme='green' isChecked={isDone}>
+        { title }
+      </Checkbox>
       <Button onClick={() => onDelete(id)} size='base' colorScheme='red' variant='outline'>
         <DeleteIcon />
       </Button>
